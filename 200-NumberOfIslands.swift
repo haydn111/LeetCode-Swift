3abbd8b49d8093f43011767ba87c0b1d13a6aed4
Su Yan <haydn111@msn.cn>
2019-08-27 23:11:52 -0700

    200

--- 200-NumberOfIslands.swift
+++ 200-NumberOfIslands.swift
@@ -20,32 +20,36 @@
 
 // Output: 3
 
-class Solution {
-    func numIslands(_ grid: [[Character]]) -> Int {
-        if grid.count == 0 || grid[0].count == 0 { return 0 }
-        var count = 0
-        var myGrid = grid
-        for i in 0...myGrid.count - 1 {
-            for j in 0...myGrid[i].count - 1 {
-                if myGrid[i][j] == "1" {
-                    count += 1
-                    bfs(&myGrid, i, j)
-                }
+
+func numIslands(_ grid: [[Character]]) -> Int {
+    if grid.count == 0 || grid[0].count == 0 { 
+        return 0 
+    }
+    var count = 0
+    var mGrid = grid
+    for i in 0...mGrid.count - 1 {
+        for j in 0...mGrid[i].count - 1 {
+            if mGrid[i][j] == "1" {
+                count += 1
+                bfs(&mGrid, i, j)
             }
         }
-        return count
     }
-    
-    func bfs(_ grid: inout [[Character]], _ i: Int, _ j: Int) { // eat an island using bfs
-        if !isValid(grid, i, j) || grid[i][j] == "0" { return }
-        grid[i][j] = "0"
-        bfs(&grid, i - 1, j)
-        bfs(&grid, i + 1, j)
-        bfs(&grid, i, j - 1)
-        bfs(&grid, i, j + 1)
+    return count
+}
+
+// eat an island using bfs
+func bfs(_ grid: inout [[Character]], _ i: Int, _ j: Int) { 
+    if !isValid(grid, i, j) || grid[i][j] == "0" { 
+        return 
     }
-    
-    func isValid(_ grid: [[Character]], _ i: Int, _ j: Int) -> Bool {
-        return i >= 0 && i < grid.count && j >= 0 && j < grid[i].count
-    }
+    grid[i][j] = "0"
+    bfs(&grid, i - 1, j)
+    bfs(&grid, i + 1, j)
+    bfs(&grid, i, j - 1)
+    bfs(&grid, i, j + 1)
+}
+
+func isValid(_ grid: [[Character]], _ i: Int, _ j: Int) -> Bool {
+    return i >= 0 && i < grid.count && j >= 0 && j < grid[i].count
 }
