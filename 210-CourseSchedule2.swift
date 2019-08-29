a80f96fffad1cb39980667bab7fe172c328fac76
Su Yan <haydn111@msn.cn>
2019-08-28 21:11:53 -0700

    210

--- 210-CourseSchedule2.swift
+++ 210-CourseSchedule2.swift
@@ -19,43 +19,47 @@
 // Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
 
 
-class Solution {
-    var allVisited = Set<Int>()
-    func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
-        // build graph [course: [prereqs]] and find roots (final courses)
-        var roots = Set<Int>(0...numCourses - 1)
-        var graph = [Int: Set<Int>]()
-        for p in prerequisites {
-            if graph[p[0]] == nil { graph[p[0]] = Set() }
-            graph[p[0]]!.insert(p[1])
-            roots.remove(p[1])
+var allVisited = Set<Int>()
+func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+    // build graph [course: [prereqs]] and find roots (final courses)
+    var roots = Set<Int>(0...numCourses - 1)
+    var graph = [Int: Set<Int>]()
+    for p in prerequisites {
+        if graph[p[0]] == nil { 
+            graph[p[0]] = Set() 
         }
-                
-        // dfs for each course dependency tree
-        // cycle detected => deadlock, cannot finish course
-        var result = [Int]()
-        for root in roots {
-            var visited = Set<Int>()
-            if dfs(graph, root, &visited, &result) { return [] }
-        }
-        return allVisited.count == numCourses ? result : []
+        graph[p[0]]!.insert(p[1])
+        roots.remove(p[1])
     }
-    
-    func dfs(_ graph: [Int: Set<Int>], _ root: Int, _ visited: inout Set<Int>, _ result: inout [Int]) -> Bool {
-        var hasCycle = false
-        allVisited.insert(root)
-        if let prereqs = graph[root] {
-            visited.insert(root)
-            for prereq in prereqs {
-                if !visited.contains(prereq) { 
-                    hasCycle = hasCycle || dfs(graph, prereq, &visited, &result)
-                } else {
-                    return true
-                }
+            
+    // dfs for each course dependency tree
+    // cycle detected => deadlock, cannot finish course
+    var result = [Int]()
+    for root in roots {
+        var visited = Set<Int>()
+        if dfs(graph, root, &visited, &result) { 
+            return [] 
+        }
+    }
+    return allVisited.count == numCourses ? result : []
+}
+
+func dfs(_ graph: [Int: Set<Int>], _ root: Int, _ visited: inout Set<Int>, _ result: inout [Int]) -> Bool {
+    var hasCycle = false
+    allVisited.insert(root)
+    if let prereqs = graph[root] {
+        visited.insert(root)
+        for prereq in prereqs {
+            if !visited.contains(prereq) { 
+                hasCycle = hasCycle || dfs(graph, prereq, &visited, &result)
+            } else {
+                return true
             }
-            visited.remove(root)
         }
-        if !result.contains(root) { result.append(root) }
-        return hasCycle
+        visited.remove(root)
     }
+    if !result.contains(root) { 
+        result.append(root) 
+    }
+    return hasCycle
 }
